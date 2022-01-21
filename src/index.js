a29b54210004b527cff2d666056b99c228147eb3
AnnaIsah <annaisabellef@gmail.com>
2022-01-20 22:24:32 -0300

    Terminei o JS e arrumei o CSS mais um pouco

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+document.getElementById("btn-Criptografar").addEventListener("click", validar)
+document.getElementById("btn-Descriptografar").addEventListener("click", validar2)
+const cipherCode = cipher
+
+function validar() {
+
+    let elemento = document.getElementById("ipt-mensagens").value.toUpperCase();
+
+    let resposta = document.getElementById("resposta");
+    let valorEncode = cipherCode.encode(7, elemento)
+    resposta.innerHTML = valorEncode
+
+}
+
+function validar2() {
+
+    let elemento = document.getElementById("ipt-mensagens").value.toUpperCase();;
+
+    let resposta = document.getElementById("resposta");
+    let valorEncode = cipherCode.decode(7, elemento)
+    resposta.innerHTML = valorEncode
+
+
+}
